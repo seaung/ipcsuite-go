a3c1d7af1de2a9a1fe20646c61339b69642f9e8d
seaung <seaung.c0m7de@protonmail.com>
2022-01-22 00:02:10 +0800

    add coder

--- pkg/modules/nse.go
+++ pkg/modules/nse.go
@@ -8,12 +8,13 @@ import (
 	"github.com/Ullaakut/nmap"
 )
 
-func RunNse(categories string) {
+func RunNseScripts(categories string, ports string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancel()
 
 	scanner, err := nmap.NewScanner(
 		nmap.WithScripts(categories),
+		nmap.WithPorts(ports),
 		nmap.WithContext(ctx),
 	)
 
@@ -26,5 +27,23 @@ func RunNse(categories string) {
 		fmt.Println(err)
 	}
 
-	fmt.Println(result)
+	for _, r := range result.Hosts {
+		if len(r.Ports) == 0 || len(r.Addresses) == 0 {
+			continue
+		}
+
+		fmt.Printf("The Target Host : %s\n", r.Addresses[0])
+
+		for _, port := range r.Ports {
+			fmt.Printf("Protocol : %s\n", port.Protocol)
+		}
+	}
+
+	for _, r := range result.PostScripts {
+		fmt.Printf("ID : %s - Output - %s\n", r.ID, r.Output)
+
+		for _, t := range r.Tables {
+			fmt.Printf("Key : %s - Tables : %s - Element : %s\n", t.Key, t.Tables[0], t.Elements[0])
+		}
+	}
 }
---
+++ pkg/types/types.go
@@ -0,0 +1,12 @@
+package types
+
+type NsePocs struct {
+	Description string
+	Author      string
+	Licens      string
+	Categories  []string
+	Headers     []string
+	Matcher     []string
+	PortRules   []string
+	Path        []string
+}
